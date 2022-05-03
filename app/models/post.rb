45d35d15ec702d7b1a4df4fa3d970a954a0d2d44
fherrerao <fherrera0206@gmail.com>
2022-05-03 08:57:50 -0500

    Add post counter and most recent posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+  belongs_to :user
+  has_many :likes
+  has_many :comments
+
+  after_save :update_posts_counter
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
+  end
+
+  def most_recent_post
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  private :update_posts_counter
 end
