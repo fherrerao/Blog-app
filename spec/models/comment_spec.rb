85f3d6a3af1b28c6d6e6436a3460b97acb5f220e
fherrerao <fherrera0206@gmail.com>
2022-05-06 23:07:53 -0500

    Added test for comments model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+   describe 'validations' do
+    it 'Increases the number of comments by 1' do
+      user = User.new(name: 'John', posts_counter: 0)
+      user.save
+
+      post = Post.new(title: 'Title', text: 'Text', comments_counter: 0, likes_counter: 0, author_id: user.id)
+      post.save
+
+      comment = Comment.new(author_id: user.id, post_id: post.id, text: 'Text')
+      comment.save
+
+      expect(user.comments.count).to eq(1)
+   end
+  end      
 end
