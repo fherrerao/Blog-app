946f05484003cfe412f8af8ac46c1c4bea55c901
fherrerao <fherrera0206@gmail.com>
2022-05-06 23:10:52 -0500

    Solved n+1 problem for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
+    @user = User.includes(:posts, posts: [:comments, { comments: [:author] }]).find(params[:user_id])
     @post = Post.where(author_id: @user.id).order(created_at: :desc)
   end
 
   def show
-    @posts = Post.find(params[:id])
+    @posts = Post.includes(:comments, comments: [:author]).find(params[:id])
     @user_posts = @posts.most_recent_comments
   end
 
@@ -13,13 +13,17 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  def create
+  def create    
     @post = Post.new(send_params)
     @post.author_id = current_user.id
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+
     if @post.save
-      redirect_to user_posts_path(@post.author_id)
+      redirect_to user_posts_path(@post.author_id), notice: 'Post was successfully created.'
     else
-      render :new
+      render :new 
+      puts 'error'
     end
   end
 
