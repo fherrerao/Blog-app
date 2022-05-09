deb6c28a32b8e10893fb1c1f2d1ac08832bd2514
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-05-09 16:19:36 -0500

    Add fields to sign up user

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,12 +17,13 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
-    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
--- db/schema.rb
+++ db/schema.rb
@@ -59,6 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_09_195633) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
