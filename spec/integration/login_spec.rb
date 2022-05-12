2e56733b67c3f5e5b4180e08474535ef6217c073
fherrerao <fherrera0206@gmail.com>
2022-05-12 15:08:00 -0500

    Add integration test for user page

--- spec/integration/login_spec.rb
+++ spec/integration/login_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Login', type: :feature do
   describe 'New session page' do
-    before :all do
+    before :all do      
+      User.destroy_all
       User.create(name: 'name', photo: 'https://www.kindpng.com/picc/m/24-248253_user-profile-default-image-png-clipart-png-download.png',
         bio: 'some', email: 'anotheruser@example.com', password: '123456', confirmed_at: DateTime.now, posts_counter: 0)
     end
@@ -34,7 +35,7 @@ RSpec.describe 'Login', type: :feature do
     end
 
     it 'when submit after filling in the email and the password with correct data, I am redirected to the root page.' do
-      fill_in('Email', with: 'user@example.com')
+      fill_in('Email', with: 'anotheruser@example.com')
       fill_in('Password', with: '123456')
       click_button 'Log in'
       expect(page).to have_current_path(root_path)
